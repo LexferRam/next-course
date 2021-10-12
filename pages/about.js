cdafafae5a4457257c4b7b05b562a866cbd08d1d
Lexfer <lexferramirez@gmail.com>
2021-10-12 15:15:33 -0400

    finish nextjs course traversy

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Meta from '../components/Meta'
+
+const about = () => {
+    return (
+        <div>
+            <Meta title="About Page" />
+            About Page
+        </div>
+    )
+}
+
+export default about
