0b401ec68778a92a7d53e9a0beaee23b68875dae
Lexfer <lexferramirez@gmail.com>
2021-10-16 10:21:17 -0400

    SSR styled-components practice

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,41 @@
-import { server} from '../config'
-// import Head from 'next/head'
-import ArticleList from '../components/ArticleList'
+import Head from 'next/head';
+import styled, { createGlobalStyle } from 'styled-components';
 
-export default function Home({articles}) {
-
+const GlobalStyle = createGlobalStyle`
+ h1 {
+   font-size: 4rem;
+ }
+`;
+const Container = styled.div`
+  text-align: center;
+`;
+const CustomP = styled.p`
+  color:blue;
+  font-size:15px;
+`
+export default function Home({todos}) {
   return (
-    <div >
-      {/* <Head>
-        <title>
-          WebDev Lexfer
-        </title>
-        <meta name='keywords' content='web development, programing' />
-      </Head> */}
-      <ArticleList articles={articles} />
-    </div>
-  )
+    <>
+      <Head>
+        <title>SSR styled-components with Next.js Starter</title>
+      </Head>
+      <Container>
+        <GlobalStyle />
+        <h1>Hello, world!</h1>
+        {todos.map((todo,i) => (
+          <CustomP key={i}>{todo.title}</CustomP>
+        ))}
+      </Container>
+    </>
+  );
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/articles`)
+export const getServerSideProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos')
 
-  const articles = await res.json()
+  const todos = await res.json()
 
   return{
-    props:{
-      articles
-    }
+    props:{todos}
   }
-
 }
- 
-// export const getStaticProps = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-
-//   const articles = await res.json()
-
-//   return{
-//     props:{
-//       articles
-//     }
-//   }
-
-// }
